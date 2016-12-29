f96a36f5d8ce108a7af8ce969dbdc0126f207fb5
宓学强 <mixueqiang@gmail.com>
2016-12-29 11:40:00 +0800

    N/A

--- web/yike-web/src/main/webapp/js/yike.js
+++ web/yike-web/src/main/webapp/js/yike.js
@@ -1,42 +1,61 @@
 // Message.
-var Message = Message
-    || {
-      info : function(msg, auto_close, $target) {
-        if ($('#message').length > 0) {
-          $('#message').remove();
-        }
+var Message = Message || {
+  info : function(msg, auto_close, target, isBefore) {
+    if ($('#message').length > 0) {
+      $('#message').remove();
+    }
 
-        if (!$target) {
-          $target = $('#header');
-        }
+    var $target;
+    if (target) {
+      $target = target;
 
-        $target
-            .after('<div id="message" style="display: none;"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span><span>'
-                + msg + '</span></div>');
-        $('#message').addClass('alert').addClass('alert-info').slideDown(200);
-        if (auto_close) {
-          setTimeout(function() {
-            $('#message').slideUp(200);
-          }, 3000);
-        }
-      },
-      error : function(msg, auto_close, $target) {
-        if ($('#message').length > 0) {
-          $('#message').remove();
-        }
-
-        if (!$target) {
-          $target = $('#header');
-        }
-
-        $target
-            .after('<div id="message" style="display: none;"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span><span>'
-                + msg + '</span></div>');
-        $('#message').addClass('alert').addClass('alert-danger').slideDown(200);
-        if (auto_close) {
-          setTimeout(function() {
-            $('#message').slideUp(200);
-          }, 3000);
-        }
+    } else {
+      $target = $('.bs-docs-header');
+      if ($target.length == 0) {
+        $target = $('#header');
       }
-    };
+    }
+
+    if (isBefore) {
+      $target.before('<div id="message" style="display: none;"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>&nbsp;' + msg + '</div>');
+    } else {
+      $target.after('<div id="message" style="display: none;"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>&nbsp;' + msg + '</div>');
+    }
+    $('#message').addClass('alert').addClass('alert-success').slideDown(300);
+
+    if (auto_close) {
+      setTimeout(function() {
+        $('#message').slideUp(300);
+      }, 5000);
+    }
+  },
+  error : function(msg, auto_close, target, isBefore) {
+    if ($('#message').length > 0) {
+      $('#message').remove();
+    }
+
+    var $target;
+    if (target) {
+      $target = target;
+
+    } else {
+      $target = $('.bs-docs-header');
+      if ($target.length == 0) {
+        $target = $('#header');
+      }
+    }
+
+    if (isBefore) {
+      $target.before('<div id="message" style="display: none;"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>&nbsp;' + msg + '</div>');
+    } else {
+      $target.after('<div id="message" style="display: none;"><span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>&nbsp;' + msg + '</div>');
+    }
+    $('#message').addClass('alert').addClass('alert-danger').slideDown(300);
+
+    if (auto_close) {
+      setTimeout(function() {
+        $('#message').slideUp(300);
+      }, 5000);
+    }
+  }
+};

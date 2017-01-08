cc419b515d0680937e041cbb385aabe457207a27
iLakeYC <15076610315@sina.cn>
2017-01-08 12:17:11 +0800

    N/A

--- web/yike-web/src/main/java/com/yike/web/dashboard/DashboardIndexResource.java
+++ web/yike-web/src/main/java/com/yike/web/dashboard/DashboardIndexResource.java
@@ -28,7 +28,7 @@ public class DashboardIndexResource extends BaseResource {
     if (user == null) {
       return signinAndGoto(request.getRequestURI());
     }
-
+    request.setAttribute("isInstructor", user.isInstructor());
     return redirect("/courses");
   }
 

b4be6f9d91f78e0095f28eb116fcc39c18c10168
iLakeYC <15076610315@sina.cn>
2017-01-10 16:09:36 +0800

    rename

--- weapp/pages/index/index.js
+++ weapp/pages/index/index.js
@@ -7,19 +7,26 @@ Page({
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
+  viewTapToFoo: function () {
+    wx.navigateTo({
+      url: '../foo/foo'
+    })
+  },
+
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
+
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }

dacb3dd35514bf3380a0127d5741454fa5b32528
iLakeYC <15076610315@sina.cn>
2017-02-09 12:36:44 +0800

    N/A

--- web/yike-web/src/main/java/com/yike/service/WxService.java
+++ web/yike-web/src/main/java/com/yike/service/WxService.java
@@ -43,12 +43,6 @@ public class WxService implements Runnable {
   @Override
   public void run() {
     requestAccessToken();
-    try {
-      Thread.currentThread().sleep(1000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    System.out.println(WX_ACCESS_TOKEN);
     setButtons();
   }
 
@@ -142,23 +136,13 @@ public class WxService implements Runnable {
     String result = "";
     try {
       URL realUrl = new URL(url);
-      // 打开和URL之间的连接
       URLConnection conn = realUrl.openConnection();
-      // 设置通用的请求属性 注意Authorization生成
-      // conn.setRequestProperty("Content-Type",
-      // "application/x-www-form-urlencoded");
-      // 发送POST请求必须设置如下两行
       conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
       conn.setDoOutput(true);
       conn.setDoInput(true);
-      // 获取URLConnection对象对应的输出流
       out = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(),"utf-8"));
-      // 发送请求参数
       System.out.println(param);
-      out.print(param);
-      // flush输出流的缓冲
       out.flush();
-      // 定义BufferedReader输入流来读取URL的响应
       in = new BufferedReader(
               new InputStreamReader(conn.getInputStream(),"utf-8"));
       String line;
@@ -166,7 +150,6 @@ public class WxService implements Runnable {
         result += line;
       }
     } catch (Exception e) {
-      System.out.println("发送 POST 请求出现异常！" + e);
       e.printStackTrace();
     }
     finally {

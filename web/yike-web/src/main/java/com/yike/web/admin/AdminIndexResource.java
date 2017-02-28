fa10d6a826499dbe86ee974cf022193fcc059850
iLakeYC <15076610315@sina.cn>
2017-02-28 16:27:21 +0800

    n/a

--- web/yike-web/src/main/java/com/yike/web/admin/AdminIndexResource.java
+++ web/yike-web/src/main/java/com/yike/web/admin/AdminIndexResource.java
@@ -1,17 +1,16 @@
 package com.yike.web.admin;
 
+import com.sun.jersey.api.view.Viewable;
+import com.yike.web.BaseResource;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-import com.sun.jersey.api.view.Viewable;
-import com.yike.web.BaseResource;
 
 /**
  * @author mixueqiang
@@ -26,8 +25,8 @@ public class AdminIndexResource extends BaseResource {
   @Produces(MediaType.TEXT_HTML)
   public Response index() {
     long userCount = entityDao.count("user");
-    long itUserCount = entityDao.count("wx_user");
-    long yikeUserCount = entityDao.count("wx_yike_user");
+      long itUserCount = entityDao.count("wx_user", "subscribe", 1);
+      long yikeUserCount = entityDao.count("wx_yike_user", "subscribe", 1);
     request.setAttribute("userCount", userCount);
     request.setAttribute("itUserCount", itUserCount);
     request.setAttribute("yikeUserCount", yikeUserCount);
